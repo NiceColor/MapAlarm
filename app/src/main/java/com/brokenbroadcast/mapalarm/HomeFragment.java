785e73c556a3c206e3b4b9af98f8ebf421619e0f
Steven Shin <steven.shin.95@gmail.com>
2016-02-05 20:53:55 -0800

    first commit, UI framework

---
+++ app/src/main/java/com/brokenbroadcast/mapalarm/HomeFragment.java
@@ -0,0 +1,52 @@
+package com.brokenbroadcast.mapalarm;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by steven on 2/5/2016.
+ */
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        Activity a;
+
+        if (context instanceof Activity){
+            a=(Activity) context;
+        }
+
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
