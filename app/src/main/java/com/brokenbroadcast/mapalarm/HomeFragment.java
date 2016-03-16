ace3ef9950eba0692e92602832a899d9fdf97cf5
Steven Shin <steven.shin.95@gmail.com>
2016-03-16 08:44:11 -0700

    updated scheduler and radius detector

--- app/src/main/java/com/brokenbroadcast/mapalarm/HomeFragment.java
+++ app/src/main/java/com/brokenbroadcast/mapalarm/HomeFragment.java
@@ -7,11 +7,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by steven on 2/5/2016.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
 
     public HomeFragment() {
         // Required empty public constructor
@@ -28,7 +30,8 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
-
+        Button scheduleButton = (Button) rootView.findViewById(R.id.schedule_botton);
+        scheduleButton.setOnClickListener(this);
         // Inflate the layout for this fragment
         return rootView;
     }
@@ -48,5 +51,11 @@ public class HomeFragment extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast toast = Toast.makeText(getActivity(),"The schedule has been set",Toast.LENGTH_LONG);
+        toast.show();
     }
 }
--- app/src/main/java/com/brokenbroadcast/mapalarm/MyMapFragment.java
+++ app/src/main/java/com/brokenbroadcast/mapalarm/MyMapFragment.java
@@ -1,7 +1,11 @@
 package com.brokenbroadcast.mapalarm;
 
+import android.content.Context;
 import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -18,6 +22,8 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -27,12 +33,14 @@ import java.util.zip.Inflater;
 /**
  * Created by steven on 2/5/2016.
  */
-public class MyMapFragment extends Fragment {
+public class MyMapFragment extends Fragment implements LocationListener {
 
     private SupportMapFragment mSupportMapFragment;
     private LatLng marker_latlng = new LatLng(49.2590910, -123.2139590);
-    private MarkerOptions second_marker;
-    private Marker dest;
+    private Marker marker;
+    private Circle circle;
+    private Location myLocation;
+    private LatLng my_latlng;
 
 
     @Override
@@ -58,12 +66,6 @@ public class MyMapFragment extends Fragment {
                         //noinspection ResourceType
                         googleMap.setMyLocationEnabled(true);
 
-
-                        MarkerOptions marker = new MarkerOptions().position(marker_latlng).title("Home: ");
-                        marker.draggable(true);
-                        googleMap.addMarker(marker);
-
-
                         CameraPosition cameraPosition = new CameraPosition.Builder().target(marker_latlng).zoom(15.0f).build();
                         CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
                         googleMap.moveCamera(cameraUpdate);
@@ -71,15 +73,22 @@ public class MyMapFragment extends Fragment {
                             @Override
                             public void onMapLongClick(LatLng point) {
 
-                                if (second_marker != null) {
-                                    dest.remove();
+                                if (marker != null) {
+                                    marker.remove();
+                                    circle.remove();
                                 }
-                                second_marker = new MarkerOptions().position(point)
-                                        .title("new location")
+                                marker = googleMap.addMarker(new MarkerOptions().position(point)
+                                        .title("destination")
                                         .icon(BitmapDescriptorFactory
-                                                .defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
-                                dest = googleMap.addMarker(second_marker);
+                                                .defaultMarker(BitmapDescriptorFactory.HUE_BLUE)).draggable(true));
 
+                                CircleOptions circleOptions = new CircleOptions().center(marker.getPosition()).radius(500);
+                                circle = googleMap.addCircle(circleOptions);
+
+                                Vibrator v = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
+                                long[] pattern = {0, 100, 1000, 300, 200, 100, 500, 200, 100};
+
+                                v.vibrate(pattern, -1);
                             }
                         });
 
@@ -92,5 +101,25 @@ public class MyMapFragment extends Fragment {
     }
 
 
+    @Override
+    public void onLocationChanged(Location location) {
+        myLocation = location;
+        my_latlng = new LatLng(location.getLatitude(), location.getLongitude());
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
 }
 
